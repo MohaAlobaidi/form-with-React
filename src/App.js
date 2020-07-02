4e412ebaf4978a9ba780c016445e26e3a52c5ea7
MOHAMMED <mohammed@MOHAMMEDs-MacBook-Pro.local>
2020-07-02 15:14:02 +0200

    first upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      name:'',
+      description:'',
+      skills:''
+    }
+  }
+
+  handleChange = (e)=>{
+    console.log(e.target.value)
+this.setState({name : e.target.value})
+  }
+
+  handelDescription = (e)=>{
+    this.setState({description : e.target.value})
+  }
+
+  handleChooisChange = (e)=>{
+    this.setState({
+      skills:e.target.value
+    })
+  }
+
+
+  handleSubmit = (e)=>{
+e.preventDefault()
+this.setState({
+  name:this.state.name,
+  description:this.state.description,
+  skills:this.state.skills
+})
+  }
+
+  render(){
+      const {name,description,skills} = this.state
+    return (
+      <div className="App">
+   
+            <form onSubmit={this.handleSubmit}> 
+             <div>
+                <label>Enter your name</label>
+                <input type='text' onChange={this.handleChange} value={name}/>
+               
+            </div>
+
+            <div>
+                <lable>descripe your self</lable>
+              <textarea onChange={this.handelDescription}></textarea>
+             
+            </div>
+
+                    <div>
+                        <label> select your skills</label>
+                        <select onChange={this.handleChooisChange}>
+                        <option value='react'> react</option>
+                        <option value='Angular'>Angular</option>
+                        <option value="vue">vue</option>
+                        </select>
+                    </div>
+
+
+      <button>submit</button>
+      <p>you are {name}</p>
+      <p> about u {description}</p>
+        <p>skills is {skills}</p>
+
+        </form>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
